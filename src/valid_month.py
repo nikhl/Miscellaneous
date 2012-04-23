5ad4b4878f63b349e1155e92cad1b37407574dd3
Nikhil Lingutla <nikhiltejlv@gmail.com>
2012-04-22 23:57:14 -0700

    Added valid_month.py

---
+++ src/valid_month.py
@@ -0,0 +1,29 @@
+# takes as input any text and if it the name of a month, the following script returns it in a neatly formatted way
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+          
+def valid_month(month):
+    if month != None and len(month) > 1:
+        list_month = list(month)
+        first_char = [x.upper() for x in list_month[0]]
+        rem_letters = [x.lower() for x in list_month[1:]]
+        modified_month = "".join(first_char+rem_letters)
+        if modified_month in months:
+            return modified_month
+        else:
+            return None
+    else:
+        return None
+    
+print valid_month("January")
